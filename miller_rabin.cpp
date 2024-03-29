b8dbe9d8ad37a2cf28bce628e71e2dbaabc6fe3f
Nguyen Thanh Quan <quannguyenthanh558@gmail.com>
2024-03-29 12:59:06 +0700

    update benchmark func

--- miller_rabin.cpp
+++ miller_rabin.cpp
@@ -8,6 +8,7 @@
 // --------------------------------------
 
 #include <iostream>
+#include <iomanip>
 #include <time.h>
 #include <chrono>
 #include <fstream>
@@ -109,92 +110,137 @@ bool miller_rabin(long long n, int k)
    return true;
 }
 
+template <typename T>
+long double average(T arr[], long long size)
+{
+   long double sum = 0.0;
+
+   // Calculate sum of all elements
+   for (long long i = 0; i < size; ++i)
+      sum += arr[i];
+
+   // Calculate average
+   return sum / size;
+}
+
 // ======================
 // >>>>>> Benchmark <<<<<
-void benchmark()
+void benchmark(int number_of_benchmark_round)
 {
-   cout << "Benchmarking... " << flush;
-
    int number_of_testing_round = 3;
-   double total_runtime = 0;
-   long long total_test = 0;
-   long long total_correct_output = 0;
-   long long checking_number = 0;
-   // long long max_check_number = 10;
-   long long max_check_number = 1000000000;
-   // string datasets_file = "./dataset/test.txt";
-   string datasets_file = "./dataset/billion-primes.txt";
 
-   // open datasets file
-   ifstream file(datasets_file);
+   double total_benchmark_runtime = 0;
+   double total_runtime[number_of_benchmark_round];
+   long long total_test[number_of_benchmark_round];
+   long long total_correct_output[number_of_benchmark_round];
 
-   if (!file.is_open())
+   cout << "================[ BENCHMARK ]=================" << endl;
+   cout << "                     ***" << endl;
+
+   // run round benchmark
+   int round = 0;
+   while (round < number_of_benchmark_round)
    {
-      cout << "[!] Datasets file does not exist." << endl;
+      cout << "[+] Benchmarking round <" << round + 1 << ">..." << flush;
+
+      total_runtime[round] = 0;
+      total_test[round] = 0;
+      total_correct_output[round] = 0;
+
+      long long checking_number = 0;
+      // long long max_check_number = 10;
+      long long max_check_number = 1000000000;
+      // string datasets_file = "./dataset/test.txt";
+      string datasets_file = "./dataset/billion-primes.txt";
+
+      // open datasets file
+      ifstream file(datasets_file);
+
+      if (!file.is_open())
+      {
+         cout << "[!] Datasets file does not exist." << endl;
+         file.close();
+         return;
+      }
+
+      string line;
+      getline(file, line);
+      long long prime = stoll(line);
+
+      // benchmark
+      auto start_benchmark_clock = high_resolution_clock::now();
+      while (checking_number < max_check_number)
+      {
+         checking_number++;
+         total_test[round]++;
+
+         auto start_testing_clock = high_resolution_clock::now();
+         bool is_checking_number_prime = miller_rabin(checking_number, number_of_testing_round);
+         auto end_testing_clock = high_resolution_clock::now();
+
+         // Calculate miller-rabin runtime (ms)
+         duration<double, milli> testing_runtime = end_testing_clock - start_testing_clock;
+
+         // check if miller-rabin return correct output
+         if ((is_checking_number_prime && checking_number == prime) || (!is_checking_number_prime && checking_number != prime))
+            total_correct_output[round]++;
+
+         // update new prime in file
+         if ((checking_number >= prime) && getline(file, line))
+            prime = stoll(line);
+
+         total_runtime[round] += testing_runtime.count();
+      }
+
+      auto end_benchmark_clock = high_resolution_clock::now();
+
+      // Calculate benchmark runtime (ms) of round round
+      duration<double, milli> benchmark_runtime = end_benchmark_clock - start_benchmark_clock;
+      total_benchmark_runtime += benchmark_runtime.count();
+
+      cout << fixed << setprecision(6) << "Done! - Run in " << benchmark_runtime.count() << " (ms)" << endl;
+
       file.close();
-      return;
+
+      round++;
    }
 
-   string line;
-   getline(file, line);
-   long long prime = stoll(line);
+   long double avg_total_test = average(total_test, number_of_benchmark_round);
+   long double avg_total_correct_output = average(total_correct_output, number_of_benchmark_round);
+   long double avg_total_runtime = average(total_runtime, number_of_benchmark_round);
 
-   // benchmark
-   while (checking_number < max_check_number)
-   {
-      checking_number++;
-      total_test++;
+   cout << "----------------------------------------------" << endl;
+   cout << "Total runtime for " << number_of_benchmark_round << " benchmark rounds: " << total_benchmark_runtime << " (ms)" << endl;
 
-      auto start_clock = high_resolution_clock::now();
-      bool is_checking_number_prime = miller_rabin(checking_number, number_of_testing_round);
-      auto end_clock = high_resolution_clock::now();
+   cout << "\n================[ STATISTICS ]================" << endl;
+   cout << "            *** Average result ***" << endl;
+   cout << "ROUND" << endl;
+   cout << fixed << setprecision(0) << "[+] Total test     : " << avg_total_test << endl;
+   cout << fixed << setprecision(0) << "[+] Total correct  : " << avg_total_correct_output << endl;
+   cout << fixed << setprecision(6) << "[+] Total runtime  : " << avg_total_runtime << " (ms)" << endl;
+   cout << "----------------------------------------------" << endl;
 
-      // Calculate miller-rabin runtime (ms)
-      duration<double, milli> runtime = end_clock - start_clock;
-
-      // check if miller-rabin return correct output
-      if ((is_checking_number_prime && checking_number == prime) || (!is_checking_number_prime && checking_number != prime))
-         total_correct_output++;
-
-      // update new prime in file
-      if ((checking_number >= prime) && getline(file, line))
-         prime = stoll(line);
-
-      total_runtime += runtime.count();
-
-      // cout << " Number: " << checking_number << ", is prime: " << (is_checking_number_prime ? "Yes" : "No") << ", Time: " << runtime.count() << "ms - total_correct:" << total_correct_output << "\n";
-   }
-
-   file.close();
-
-   cout << "Done !" << endl
-        << endl;
-
-   cout << "=================================" << endl;
-   cout << "- Total test        : " << total_test << endl;
-   cout << "- Total correct     : " << total_correct_output << endl;
-   cout << "- Total runtime     : " << total_runtime << " ms" << endl;
-   cout << "=================================" << endl;
-   cout << "[>] Average runtime: " << total_runtime / total_test << " ms" << endl;
-   cout << "[>] Average correct: " << float(total_correct_output) / total_test * 100 << "%" << endl;
+   cout << "ALGORITHM" << endl;
+   cout << fixed << setprecision(6) << "[+] Average runtime: " << float(avg_total_runtime) / avg_total_test << " (ms)" << endl;
+   cout << fixed << setprecision(6) << "[+] Average correct: " << float(avg_total_correct_output) / avg_total_test * 100 << "%" << endl;
 }
 
 // ======================
 // >>>>>>> Main <<<<<<
 int main()
 {
-   cout << "  /=================================\\" << endl;
-   cout << "  |                                 |" << endl;
-   cout << "  |          MILLER RABIN           |" << endl;
-   cout << "  |      ____________________       |" << endl;
-   cout << "  |  Probabilistic primality test   |" << endl;
-   cout << "  |                                 |" << endl;
-   cout << "  \\=================================/" << endl;
-   cout << "       \\_______QuanBlue_______/" << endl
+   cout << "      /=================================\\" << endl;
+   cout << "      |                                 |" << endl;
+   cout << "      |          MILLER RABIN           |" << endl;
+   cout << "      |      ____________________       |" << endl;
+   cout << "      |  Probabilistic primality test   |" << endl;
+   cout << "      |                                 |" << endl;
+   cout << "      \\=================================/" << endl;
+   cout << "           \\_______QuanBlue_______/" << endl
         << endl;
 
    srand(time(NULL));
-   benchmark();
+   benchmark(20);
 
    return 0;
 }
